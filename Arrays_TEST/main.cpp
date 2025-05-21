29839fae22d32bee0cfd968d3216f542b0f6f776
Юля <@DESKTOP-LDN2BMM>
2025-05-21 21:21:46 +0300

    overload for double

--- Arrays_TEST/main.cpp
+++ Arrays_TEST/main.cpp
@@ -46,7 +46,7 @@ double Max(double arr[ROWS][COLS], const int ROWS, const int COLS);
 void  ShiftLeft(int arr[], const int n, const int shifts); // сдвиг влево
 void  ShiftLeft(double arr[], const int n, const int shifts);
 void  ShiftRight(int arr[], const int n, const int shifts);
-void  ShiftRight(double arr[], const int n, const int shifts);
+void  ShiftRight(double arr[], const int n, const int shifts); 
 
 void ShiftLeft(int arr[ROWS][COLS], const int ROWS, const int COLS, int shifts);
 
